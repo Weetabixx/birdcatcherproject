e3aaa45e4f8d6fb2ccb799f69455cafa5dd320b1
waikii <wai.kai.wu.14@aberdeen.ac.uk>
2017-04-05 14:48:08 +0000

    added search function to accounts on admin side, hopefully no more needs done now?

--- twitter_stream/admin.py
+++ twitter_stream/admin.py
@@ -9,7 +9,12 @@ class TweetAdmin(admin.ModelAdmin): # lets you search for tweets on the admin pa
     search_fields=['tweet_text','tweet_handle','tweet_created','tweet_pin']
     readonly_fields = ('tweet_handle', 'tweet_text', 'tweet_created',)
     
+class AccountAdmin(admin.ModelAdmin): # lets you search for tweets on the admin page
+    list_display=('account_handle','account_Name','account_group','filter_by_hashtags',)
+    search_fields=['account_handle','account_Name','account_group__group_name']
+    readonly_fields = ('account_handle','account_Name')
+    
 admin.site.register(tweet,TweetAdmin)
-admin.site.register(account) 
+admin.site.register(account,AccountAdmin) 
 admin.site.register(hashtag)
 admin.site.register(group)
